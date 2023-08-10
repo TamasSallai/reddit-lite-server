3a4d40cc5872ceb6e1a8d13dee452c6a0c38c547
Tamas Sallai <tamas.sallai.hu@gmail.com>
2023-08-10 14:19:37 +0200

    added crud schema and resolvers for post

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,35 @@
 import { Resolvers } from "./generated/graphql"
+import prisma from "./prisma"
 
 const resolvers: Resolvers = {
   Query: {
-    hello: () => "world",
+    getPost: (_, { id }) => prisma.post.findUnique({ where: { id } }),
+    getPosts: () => prisma.post.findMany(),
+  },
+
+  Mutation: {
+    createPost: (_, { title }) => prisma.post.create({ data: { title } }),
+    updatePost: async (_, { id, title }) => {
+      const post = await prisma.post.findUnique({ where: { id } })
+
+      if (!post) return null
+
+      return prisma.post.update({
+        where: { id },
+        data: {
+          title: title || undefined,
+        },
+      })
+    },
+    deletePost: async (_, { id }) => {
+      const post = await prisma.post.findFirst({ where: { id } })
+
+      if (!post) return null
+
+      await prisma.post.delete({ where: { id } })
+
+      return true
+    },
   },
 }
 
